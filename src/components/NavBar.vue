d2ab0e99ae9b85f22a8b93712e9d756f027871cc
HiroLiang <hiromichi.liang@gmail.com>
2023-08-21 17:47:55 +0800

    Add navbar
    give prop to show different nav title

---
+++ src/components/NavBar.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const props = defineProps({
+    ProfilePicture: {
+        type: String,
+        default: 'user-interface.png'
+    },
+    NavBarList: {
+        default: [
+            { title: '食譜', toUrl: '/recipes' },
+            { title: '課程', toUrl: '/course' },
+            { title: '商品', toUrl: '/products' }
+        ]
+    }
+});
+</script>
+<template>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <router-link class="navbar-brand" to="/">Dessert Oasis</router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link active" aria-current="page" to="/about">首頁</router-link>
+                    </li>
+                    <li v-for="navTitle in props.NavBarList" class="nav-item">
+                        <router-link :to="navTitle.toUrl" class="nav-link" href="#">{{ navTitle.title }}</router-link>
+                    </li>
+                </ul>
+                <span class="loginIcon">
+                    <router-link to="/signin">
+                        <img :src="`/images/navbar/${props.ProfilePicture}`" alt="ProfilePicture">
+                    </router-link>
+                </span>
+            </div>
+        </div>
+    </nav>
+</template>
+<style scoped>
+.loginIcon {
+    width: 32px;
+    height: 32px;
+    border-radius: 16px;
+}
+
+.loginIcon img {
+    width: 32px;
+    height: 32px;
+    border-radius: 16px;
+}
+</style>

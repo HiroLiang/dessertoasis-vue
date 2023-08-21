85ad7136e280f6da48c266511595b23c636a6588
HiroLiang <hiromichi.liang@gmail.com>
2023-08-21 16:45:29 +0800

    no message

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import request from "./reauest";
 
 //Test
 const reqTest = () => {
-    return request.post('/test2', `{"name":"John"}`, { headers: { 'Content-Type': 'application/json' } })
+    return request.post('/test', `{"name":"John"}`, { headers: { 'Content-Type': 'application/json' } })
 }
 
 export { reqTest }

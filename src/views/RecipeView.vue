60a2bfcf7a76b4b32197a5772cb8fa5c922ec925
CR4-21\iSpan <s83582003@gmail.com>
2023-08-22 16:45:32 +0800

    更新RecipeView

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -4,8 +4,8 @@ import Carousel from '@/components/Carousel.vue'
 import { reactive } from 'vue'
 const navbarlist = reactive([
     { title: '食譜', toUrl: '/recipes' },
-    { title: '課程', toUrl: '/' },
-    { title: '商品', toUrl: '/' },
+    { title: '課程', toUrl: '/courses' },
+    { title: '商品', toUrl: '/product' },
     { title: '建立食譜', toUrl: '/' }
 ])
 </script>
@@ -14,11 +14,42 @@ const navbarlist = reactive([
     <NavBar :NavBarList="navbarlist" />
     <div>
         <h1> This is recipe homepage </h1>
+        <hr>
     </div>
-    <hr>
     <div class="container">
-        <Carousel />
+        <div class="txtwrap">
+            <h2 class="titleTxt">每周熱門食譜</h2>
+        </div>
+        <div class="container border pr-10 pl-10">
+            <Carousel />
+        </div>
     </div>
 </template>
 
-<style></style>
+<style>
+/*調整食譜縮圖列外框線位置*/
+.recipeListContainer {
+    margin: 0;
+    padding-left: 60px;
+    padding-right: 60px;
+    display: block;
+    align-items: center;
+    flex-wrap: wrap;
+}
+
+.txtwrap {
+    max-width: 250px;
+    display: block;
+    align-items: center;
+    text-align: center;
+}
+
+.titleTxt {
+    display: block;
+    border: 1px solid black;
+    border-radius: 10px 10px 0px 0px;
+    border-bottom: none;
+    padding: 10px;
+    margin-top: 50px;
+}
+</style>

e65e88aeecddfc14e81eb1d5887dbc5ddc126b16
hhc93 <clarahc30@gmail.com>
2023-08-23 21:45:48 +0800

    新增課程卡片、分頁 component

---
+++ src/components/CourseCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="col-12 col-md-3 mb-4">
+    <div class="card">
+      <img
+        src="../assets/images/course/cupcake.jpg"
+        class="card-img-top"
+        alt=""
+      />
+      <div class="card-body">
+        <h5 class="card-title">杯子蛋糕</h5>
+        <p class="card-text">滿足你的甜蜜渴望，一個杯子，一份幸福。</p>
+        <a href="#" class="btn btn-primary">了解更多</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts"></script>
+<style scoped></style>

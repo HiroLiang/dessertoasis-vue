5538188a430ebed9ba483e4fd558e2040be25a6a
CR4-21\iSpan <s83582003@gmail.com>
2023-08-24 17:02:41 +0800

    更新食譜建立頁面

--- src/views/RecipeCreatePage.vue
+++ src/views/RecipeCreatePage.vue
@@ -52,7 +52,28 @@
                     </div>
                 </div>
             </div>
-            <div class="recipeStepsContainer container">
+            <div class="container ml-3">
+                <div class="ingredientContainer row justify-content-start  ">
+                    <div class="ingredientTypeContainer col-4">
+                        <label for="ingredientName" class="form-label">所需食材</label><br>
+                        <input class="form-control" type="search" id="ingredientName" name="ingredientName"
+                            placeholder="請輸入食材名稱">
+                    </div>
+                    <div class="ingredientQuantityContainer col-4">
+                        <label for="ingredientQuantity" class="form-label">食材份量</label><br>
+                        <input class="form-control" type="text" id="ingredientQuantity" name="ingredientQuantity"
+                            required="required" placeholder="份量">
+                    </div>
+                    <div class="ingrediantBtn col-2 mt-4">
+                        <button class="btn btn-light ">刪除</button>
+                        <button class="btn btn-light ">移動</button>
+                    </div>
+                </div>
+                <div class="newRecipeStepContainer d-grid mt-3">
+                    <button class="btn btn-light">+ 增加食材</button>
+                </div>
+            </div>
+            <div class="recipeStepsContainer container mt-3">
                 <div class="step1Container row justify-content-between">
                     <div class="stepImgContainer col-2 align-self-center">
                         <label for="recipeStep1" class="form-label">步驟1</label><br>

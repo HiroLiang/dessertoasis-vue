bf68078eacfef7324e3efd6daf2f6729a8149928
hhc93 <clarahc30@gmail.com>
2023-08-25 00:17:54 +0800

    新增課程後台表格傳值測試

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,16 +2,25 @@ import request from "./request";
 
 //Test
 const reqTest = () => {
-    return request.post('/test', `{"name":"John"}`, { headers: { 'Content-Type': 'application/json' } })
-}
+  return request.post("/test", `{"name":"John"}`, {
+    headers: { "Content-Type": "application/json" },
+  });
+};
 
 //CMS食譜測試
 const reqDatas = () => {
-    return request.get('/recipe/all')
-}
+  return request.get("/recipe/all");
+};
 
 const getReservations = (room: number, start: string, end: string) => {
-    return request.get(`/reservation/getinmonth?room=${room}&start=${start}&end=${end}`)
-} 
+  return request.get(
+    `/reservation/getinmonth?room=${room}&start=${start}&end=${end}`
+  );
+};
 
-export { reqTest, reqDatas, getReservations }
+//從controller拿到所有課程，export出Promise物件給vue?
+const getAllCourses = () => {
+  return request.get("/course/all");
+};
+
+export { reqTest, reqDatas, getReservations, getAllCourses };

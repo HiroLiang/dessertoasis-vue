0a54dee995a1c6a3f949a8d9c93fdb8f186cf892
Annelise-Wang <b9605168@gmail.com>
2023-08-25 11:28:16 +0800

    no message

--- src/views/product/Product.vue
+++ src/views/product/Product.vue
@@ -19,14 +19,15 @@ import ProductCard from '@/components/ProductCard.vue';
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active headerpic" data-bs-interval="10000">
-                <img src="../assets/images/headerShow/toa-heftiba-1o_FCxY-VH8-unsplash.jpg" class="d-block w-100" alt="#">
-            </div>
-            <div class="carousel-item headerpic" data-bs-interval="2000">
-                <img src="../assets/images/headerShow/michal-parzuchowski-pMsvOrnIF3Y-unsplash.jpg" class="d-block w-100"
+                <img src="../../assets/images/headerShow/toa-heftiba-1o_FCxY-VH8-unsplash.jpg" class="d-block w-100"
                     alt="#">
             </div>
             <div class="carousel-item headerpic" data-bs-interval="2000">
-                <img src="../assets/images/headerShow/nick-karvounis-jN_M0McVNeI-unsplash.jpg" class="d-block w-100"
+                <img src="../../assets/images/headerShow/michal-parzuchowski-pMsvOrnIF3Y-unsplash.jpg" class="d-block w-100"
+                    alt="#">
+            </div>
+            <div class="carousel-item headerpic" data-bs-interval="2000">
+                <img src="../../assets/images/headerShow/nick-karvounis-jN_M0McVNeI-unsplash.jpg" class="d-block w-100"
                     alt="#">
             </div>
         </div>

9ee97943ef87f8e531c0f4195118c04d52636644
HiroLiang <hiromichi.liang@gmail.com>
2023-08-25 10:39:21 +0800

    no message

--- src/api/index.ts
+++
@@ -1,8 +0,0 @@
-import request from "./request";
-
-//Test
-const reqTest = () => {
-    return request.post('/test2', `{"name":"John"}`, { headers: { 'Content-Type': 'application/json' } })
-}
-
-export { reqTest }

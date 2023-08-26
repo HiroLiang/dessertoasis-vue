7d12fab970d2ac5fa7951d4ab0d295166a8d0cfa
hhc93 <clarahc30@gmail.com>
2023-08-26 19:56:03 +0800

    no message

--- src/views/AddCourse.vue
+++ src/views/AddCourse.vue
@@ -4,6 +4,15 @@
     <div v-if="isTeacher">
       <h1>新增課程</h1>
       <!-- 这里放置新增课程的表单或其他内容 -->
+
+      <form @submit.prevent="addCourse">
+        <div>
+          <label for="courseName">课程名称:</label>
+          <input type="text" id="courseName" v-model="courseName" />
+        </div>
+        <!-- 其他课程信息字段 -->
+        <button type="submit">提交</button>
+      </form>
     </div>
     <div v-else>
       <p>您不是教師，無法前往此頁。</p>
@@ -23,6 +32,7 @@ onMounted(async () => {
     const response = await checkTeacherStatus()
     if (response.data === "Teacher cookie set successfully.") {
       // 用戶是老師，顯示新增課程内容
+      console.log(response)
       isTeacher.value = true
     }
   } catch (error) {

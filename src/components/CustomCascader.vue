15311f28a97ef8f67556b52932c5db0c00a27aaa
Annelise-Wang <b9605168@gmail.com>
2023-08-28 15:38:38 +0800

    新增可勾選下拉選單組件

---
+++ src/components/CustomCascader.vue
@@ -0,0 +1,32 @@
+<template>
+    <n-cascader v-model:value="modelValue" :options="customOptions" :max-tag-count="maxTagCount"
+        :expand-trigger="expandTrigger" :filterable="isFilterable" :clear-filter-after-select="isClearFilterAfterSelect"
+        @update:value="handleUpdateValue" />
+</template>
+  
+<script setup>
+import { ref, defineProps } from "vue";
+import { NCascader } from "naive-ui";
+
+const props = defineProps({
+    options: Array,
+    hoverTrigger: Boolean, // 是否使用 hover 
+    value: [null, Array],
+    filterable: Boolean, // 是否可搜尋
+    clearFilterAfterSelect: Boolean // 是否在選中後清除搜尋內容
+});
+
+const modelValue = ref(props.value); // 使用 v-model
+
+const customOptions = props.options;
+const maxTagCount = props.hoverTrigger ? 'responsive' : undefined;
+const expandTrigger = props.hoverTrigger ? 'hover' : 'click';
+const isFilterable = props.filterable;
+const isClearFilterAfterSelect = props.clearFilterAfterSelect;
+
+const handleUpdateValue = (value, options) => {
+    modelValue.value = value; // 更新 v-model
+    console.log(value, options);
+};
+</script>
+  
--- src/views/product/Addproduct.vue
+++ src/views/product/Addproduct.vue
@@ -1,94 +1,39 @@
 <template>
-    <n-space vertical>
-        <!--<n-space>
-            <n-space>
-                <n-switch v-model:value="checkStrategyIsChild" />Child Check Strategy
-            </n-space>
-            <n-space><n-switch v-model:value="cascade" />Cascade</n-space>
-            <n-space><n-switch v-model:value="showPath" />Show Path</n-space>
-            <n-space><n-switch v-model:value="hoverTrigger" />Hover Trigger</n-space>
-            <n-space><n-switch v-model:value="filterable" />Filterable</n-space>
-            <n-space>
-                <n-switch v-model:value="responsiveMaxTagCount" />Responsive MaxTagCount
-            </n-space>
-            <n-space>
-                <n-switch v-model:value="clearFilterAfterSelect" />clearFilterAfterSelect
-            </n-space>
-        </n-space>-->
-        <n-cascader v-model:value="value" multiple clearable placeholder="没啥用的值"
-            :max-tag-count="responsiveMaxTagCount ? 'responsive' : undefined"
-            :expand-trigger="hoverTrigger ? 'hover' : 'click'" :options="options" :cascade="cascade"
-            :check-strategy="checkStrategyIsChild ? 'child' : 'all'" :show-path="showPath" :filterable="filterable"
-            :clear-filter-after-select="clearFilterAfterSelect" @update:value="handleUpdateValue" />
-    </n-space>
-    <div class="container">
-        <p>新增圖片和影片</p>
-        <div class="image-upload">
-            <p>新增圖片</p>
-            <input type="file" @change="addImage" accept="image/*" />
-            <div v-for="(image, index) in images" :key="index" class="uploaded-item">
-                <img :src="image.url" alt="Uploaded Image" />
-                <button @click="removeImage(index)">刪除</button>
-            </div>
-        </div>
-        <div class="video-upload">
-            <p>新增影片</p>
-            <input type="file" @change="addVideo" accept="video/*" />
-            <div v-for="(video, index) in videos" :key="index" class="uploaded-item">
-                <video controls>
-                    <source :src="video.url" type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-                <button @click="removeVideo(index)">刪除</button>
-            </div>
-        </div>
+    <div>
+        <CustomCascader :options="customOptions" :hoverTrigger="hoverTrigger" :value="value" :filterable="filterable"
+            :clearFilterAfterSelect="clearFilterAfterSelect" />
     </div>
 </template>
   
 <script setup>
-import { ref } from "vue";
-import { NSpace, NSwitch, NCascader } from 'naive-ui';
+import CustomCascader from "@/components/CustomCascader.vue";
 
-const checkStrategyIsChild = ref(false);
-const cascade = ref(false);
-const showPath = ref(true);
-const hoverTrigger = ref(true);
-const value = ref(null);
-const filterable = ref(true);
-const responsiveMaxTagCount = ref(false);
-const clearFilterAfterSelect = ref(true);
-const options = getOptions();
+const customOptions = [
+    {
+        value: "v-1",
+        label: "烘焙食材",
+        children: [
+            {
+                value: "v-1-1",
+                label: "麵粉",
+            }
+        ]
+    },
+    {
+        value: "v-2",
+        label: "包裝材料",
+        children: [
+            {
+                value: "v-2-1",
+                label: "吐司袋、麵包袋",
+            }
+        ]
+    },
+    // ... 其他选项
+];
+const hoverTrigger = true; // 是否使用hover
+const value = null; // 初始值
+const filterable = true; // 是否可搜尋
+const clearFilterAfterSelect = true; // 是否在選中後清除搜尋內容
 
-function getOptions(depth = 3, iterator = 1, prefix = "") {
-    const length = 12;
-    const options = [];
-    for (let i = 1; i <= length; ++i) {
-        if (iterator === 1) {
-            options.push({
-                value: `v-${i}`,
-                label: `l-${i}`,
-                disabled: i % 5 === 0,
-                children: getOptions(depth, iterator + 1, "" + String(i))
-            });
-        } else if (iterator === depth) {
-            options.push({
-                value: `v-${prefix}-${i}`,
-                label: `l-${prefix}-${i}`,
-                disabled: i % 5 === 0
-            });
-        } else {
-            options.push({
-                value: `v-${prefix}-${i}`,
-                label: `l-${prefix}-${i}`,
-                disabled: i % 5 === 0,
-                children: getOptions(depth, iterator + 1, `${prefix}-${i}`)
-            });
-        }
-    }
-    return options;
-}
-
-const handleUpdateValue = (value, options) => {
-    console.log(value, options);
-};
 </script>

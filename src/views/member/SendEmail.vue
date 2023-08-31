5e1412971e3d2ed8d5f3e41f2bbc69feeb8f9bf8
Han <roiipjoi@gmail.com>
2023-08-31 15:55:26 +0800

    email 測試

--- src/views/member/SendEmail.vue
+++ src/views/member/SendEmail.vue
@@ -2,34 +2,25 @@
     <div>
         <p>Member Status: {{ memberStatus }}</p>
         <button @click="changeStatus">Change Status</button>
+        <router-link to="/">回到首頁</router-link>
     </div>
 </template>
-  
+    
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+
+
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 const memberStatus = ref('INACTIVE');
-let memberId = null; // 初始化成 null
 
-// 在組件 mount 時設定 memberId，假設 memberData 是您創建帳號後的回傳物件
-onMounted(() => {
-    memberId = memberData.memberId; // 假設 memberData 包含了創建帳號後的回傳數據
-});
-
-const changeStatus = async () => {
-    try {
-        const response = await axios.put('http://localhost:8080/member/changeStatus', {
-            memberId: memberId, // 使用創建帳號的 memberId
-            newStatus: 'ACTIVE',
-        });
-
-        if (response.data === 'success') {
-            memberStatus.value = 'ACTIVE';
-        }
-    } catch (error) {
-        console.error('Error changing status:', error);
+const changeStatus = () => {
+    if (memberStatus.value === 'INACTIVE') {
+        memberStatus.value = 'ACTIVE '
+    } else {
+        memberStatus.value = 'ACTIVE '
     }
-};
+}
+
 </script>
   

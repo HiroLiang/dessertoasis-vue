60e04325bade89e18446ca128172ed41fe2bd1cf
CR4-21\iSpan <s83582003@gmail.com>
2023-08-31 18:06:32 +0800

    no message

--- src/views/recipe/RecipeBackground.vue
+++ src/views/recipe/RecipeBackground.vue
@@ -11,21 +11,14 @@ const datas = async () => {
 }
 
 const tableDatas = reactive([
-    { id: 1, recipeID: 1, recipeTitle: "大恐龍1", time: 9, createdate: new Date("2020-01-01T11:11:00") },
-    { id: 2, recipeID: 2, recipeTitle: "大恐龍2", time: 99, createdate: new Date("2021-02-01T12:30:00") },
-    { id: 3, recipeID: 3, recipeTitle: "大恐龍n", time: 999, createdate: new Date("2022-11-30T23:11:00") }])
+    { id: 1, recipeTitle: "大恐龍1", time: 9, createdate: new Date("2020-01-01T11:11:00") },
+    { id: 2, recipeTitle: "大恐龍2", time: 99, createdate: new Date("2021-02-01T12:30:00") },
+    { id: 3, recipeTitle: "大恐龍n", time: 999, createdate: new Date("2022-11-30T23:11:00") }])
 
 const title = reactive([
-    { label: "編號", key: "recipeID" },
     { label: "名稱", key: "recipeTitle" },
-    { label: "簡介", key: "createdate" },
     { label: "分類", key: "categoryID" },
     { label: "撰寫者", key: "recipeAuthorID" },
-    { label: "製作時間", key: "cookingTime" },
-    { label: "難易度", key: "difficulty" },
-    { label: "步驟內容", key: "recipeSteps" },
-    { label: "建立時間", key: "recipeCreateDate" },
-    { label: "訪問計數器", key: "recipeVisitCount" },
     { label: "公開狀態", key: "recipeStatus" },])
 
 

0b7c8ce679d5f227cec594dc0864caf625ddc940
CR4-19\iSpan <hero929408@gmail.com>
2023-09-15 17:11:06 +0800

    修改 cart

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,6 +4,7 @@ import { reqSignOut, reqUserPermission } from "../api";
 import { useRouter } from 'vue-router';
 import { googleLogout } from 'vue3-google-login';
 import { useCartStore } from "../stores/cart";
+import { NBadge, NAvatar } from "naive-ui";
 
 const storedData = localStorage.getItem('googleLoginData');
 const isGoogleLoggedIn = localStorage.getItem('googleLoggedIn');
@@ -138,7 +139,11 @@ onMounted(() => {
                 </ul>
             </div>
             <div class="mx-4">
-                <router-link to="/cart" class="dropdown-item">購物車({{ cart.count }})</router-link>
+                <router-link to="/cart" class="dropdown-item">
+                    <n-badge :value="cart.count">
+                        <n-avatar src="/images/navbar/cart.png"/>
+                    </n-badge>
+                </router-link>
             </div>
             <div class="dropdown">
                 <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown"

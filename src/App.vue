140372a3a5ad995bda92957085eb6cac8b4da90b
Imei <s83582003@gmail.com>
2023-09-17 02:11:42 +0800

    下拉選單調整完成   footer組件添加圖示及智慧財產權文字

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { RouterView } from 'vue-router';
+
 import { NBackTop, NLoadingBarProvider, NMessageProvider, NNotificationProvider } from 'naive-ui'
 
 const placement = "top-right"
+const notiPlacement = "bottom-right"
 
 </script>
 <template>
   <n-message-provider :placement="placement" :max="8">
-    <n-notification-provider>
+    <n-notification-provider :placement="notiPlacement" style="font-size: 32px;">
       <n-loading-bar-provider>
         <RouterView />
       </n-loading-bar-provider>

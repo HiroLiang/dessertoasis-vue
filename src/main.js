6e25db4f6bee6b0732d0408a2d9af71508f91f61
Han <roiipjoi@gmail.com>
2023-09-18 17:40:17 +0800

    member後台

--- src/main.js
+++ src/main.js
@@ -46,7 +46,8 @@ router.beforeEach(async (to, from, next) => {
 
     if (!isLogin.value && to.path === '/cms') {//沒登入無法進入/cms頁面，會導向login頁面。除了cms都可以進入。
         next({ name: 'logIn' })
-    } else if (!isLogin.value && to.path === '/recipes/createrecipe') {//登入且是管理員可以進入/cms頁面
+    } else if (!isLogin.value && to.path === '/recipes/createrecipe') {
+        alert('請先登入')
         next({ name: 'logIn' })
     } else if (isLogin.value && isAdmin.value && to.path === '/cms') {//登入且是管理員可以進入/cms頁面
         next()

44a667f2b4ceb6f5e82860ea4ab3a9d6a48961db
HiroLiang <hiromichi.liang@gmail.com>
2023-09-20 16:58:48 +0800

    no message

--- src/views/cms/CmsHome.vue
+++ src/views/cms/CmsHome.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import { useRouter } from "vue-router"
 import { NCard, NTabs, NTabPane, NNumberAnimation, NStatistic } from 'naive-ui'
+
+const router = useRouter()
 
 const totalVisitRef = ref(null)
 
 //定義統計資料
 const totalVisit = ref([9980, 12, 23, 3])
 
-const presonalSum = ref([1203400, 123, 56, 0])
+const presonalSum = ref([1203400, 3, 56, 0])
 
 
 
@@ -16,108 +19,106 @@ onMounted(() => {
 })
 </script>
 <template>
-    <div class="countDatas">
-        <n-card style="border-radius: 20px;">
-            <n-tabs type="line" animated>
-                <n-tab-pane name="網站統計" tab="網站統計">
-                    <div class="visitContainer">
-                        <div class="totalVisitContainer">
-                            <n-statistic label="月訪問數" tabular-nums class="dataContainer">
-                                <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="totalVisit[0]" />
-                                <template #suffix>
-                                    <span style="font-size: 12px;">次</span>
-                                </template>
-                            </n-statistic>
+    <div
+        style="background-color: rgb(235, 234, 234);padding-top: 10px; display: flex;flex-direction: column;justify-content: center;align-items: center;width: 100%;height: 100%;">
+        <div class="countDatas">
+            <n-card style="border-radius: 20px;">
+                <n-tabs type="line" animated>
+                    <n-tab-pane name="網站統計" tab="網站統計">
+                        <div class="visitContainer">
+                            <div class="totalVisitContainer" @click="router.replace({ name: 'cmsChartVisit' })">
+                                <n-statistic label="月訪問數" tabular-nums class="dataContainer">
+                                    <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="totalVisit[0]" />
+                                    <template #suffix>
+                                        <span style="font-size: 12px;">次</span>
+                                    </template>
+                                </n-statistic>
+                            </div>
+                            <div class="totalVisitContainer">
+                                <n-statistic label="可用教室" tabular-nums class="dataContainer">
+                                    <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="totalVisit[3]" />
+                                    <template #suffix>
+                                        <span style="font-size: 12px;">間</span>
+                                    </template>
+                                </n-statistic>
+                            </div>
+                            <div class="totalVisitContainer">
+                                <n-statistic label="項目總數" tabular-nums class="dataContainer">
+                                    <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="totalVisit[1]" />
+                                    <template #suffix>
+                                        <span style="font-size: 12px;">篇</span>
+                                    </template>
+                                </n-statistic>
+                            </div>
+                            <div class="totalVisitContainer">
+                                <n-statistic label="產品銷售率" tabular-nums class="dataContainer">
+                                    <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="totalVisit[2]" />
+                                    <template #suffix>
+                                        <span style="font-size: 14px;">%</span>
+                                    </template>
+                                </n-statistic>
+                            </div>
                         </div>
-                        <div class="totalVisitContainer">
-                            <n-statistic label="可用教室" tabular-nums class="dataContainer">
-                                <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="totalVisit[3]" />
-                                <template #suffix>
-                                    <span style="font-size: 12px;">間</span>
-                                </template>
-                            </n-statistic>
+                    </n-tab-pane>
+                    <n-tab-pane name="個人統計" tab="個人統計">
+                        <div class="visitContainer">
+                            <div class="totalVisitContainer">
+                                <n-statistic label="未結案件" tabular-nums class="dataContainer">
+                                    <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="presonalSum[0]" />
+                                    <template #suffix>
+                                        <span style="font-size: 12px;">件</span>
+                                    </template>
+                                </n-statistic>
+                            </div>
+                            <div class="totalVisitContainer">
+                                <n-statistic label="待確認請求" tabular-nums class="dataContainer">
+                                    <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="presonalSum[1]" />
+                                    <template #suffix>
+                                        <span style="font-size: 12px;">件</span>
+                                    </template>
+                                </n-statistic>
+                            </div>
+                            <div class="totalVisitContainer">
+                                <n-statistic label="特休剩餘" tabular-nums class="dataContainer">
+                                    <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="presonalSum[2]" />
+                                    <template #suffix>
+                                        <span style="font-size: 12px;">/小時</span>
+                                    </template>
+                                </n-statistic>
+                            </div>
+                            <div class="totalVisitContainer">
+                                <n-statistic label="待辦事項" tabular-nums class="dataContainer">
+                                    <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="presonalSum[3]" />
+                                    <template #suffix>
+                                        <span style="font-size: 12px;">件</span>
+                                    </template>
+                                </n-statistic>
+                            </div>
                         </div>
-                        <div class="totalVisitContainer">
-                            <n-statistic label="項目總數" tabular-nums class="dataContainer">
-                                <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="totalVisit[1]" />
-                                <template #suffix>
-                                    <span style="font-size: 12px;">篇</span>
-                                </template>
-                            </n-statistic>
-                        </div>
-                        <div class="totalVisitContainer">
-                            <n-statistic label="產品銷售率" tabular-nums class="dataContainer">
-                                <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="totalVisit[2]" />
-                                <template #suffix>
-                                    <span style="font-size: 14px;">%</span>
-                                </template>
-                            </n-statistic>
-                        </div>
-                    </div>
-                </n-tab-pane>
-                <n-tab-pane name="個人統計" tab="個人統計">
-                    <div class="visitContainer">
-                        <div class="totalVisitContainer">
-                            <n-statistic label="月營收" tabular-nums class="dataContainer">
-                                <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="presonalSum[0]" />
-                                <template #suffix>
-                                    <span style="font-size: 16px;">$</span>
-                                </template>
-                            </n-statistic>
-                        </div>
-                        <div class="totalVisitContainer">
-                            <n-statistic label="商品數量" tabular-nums class="dataContainer">
-                                <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="presonalSum[1]" />
-                                <template #suffix>
-                                    <span style="font-size: 16px;">$</span>
-                                </template>
-                            </n-statistic>
-                        </div>
-                        <div class="totalVisitContainer">
-                            <n-statistic label="特休剩餘" tabular-nums class="dataContainer">
-                                <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="presonalSum[2]" />
-                                <template #suffix>
-                                    <span style="font-size: 12px;">/小時</span>
-                                </template>
-                            </n-statistic>
-                        </div>
-                        <div class="totalVisitContainer">
-                            <n-statistic label="待辦事項" tabular-nums class="dataContainer">
-                                <n-number-animation ref="totalVisitRef" show-separator :from="0" :to="presonalSum[3]" />
-                                <template #suffix>
-                                    <span style="font-size: 12px;">件</span>
-                                </template>
-                            </n-statistic>
-                        </div>
-                    </div>
-                </n-tab-pane>
-            </n-tabs>
-        </n-card>
-    </div>
-    <div class="business">
-        <div class="mainBusiness">
-            <div class="productSale">
-                <h2>商品銷售圖表</h2>
-            </div>
-            <div class="commendCount">
-                <h2>評論統計</h2>
-            </div>
+                    </n-tab-pane>
+                </n-tabs>
+            </n-card>
         </div>
-        <div class="staffArea">
-            <div class="calendar">
-                <h2>日曆</h2>
-            </div>
-            <div class="schedule">
-                <h2>代辦事項</h2>
+        <div class="chartView">
+            <router-view />
+            <div class="listContainer">
+                <ul>
+                    <li>dfs</li>
+                    <li>sdf</li>
+                    <li>sdfs</li>
+                    <li>sdfs</li>
+                    <li>sdfsdf</li>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 <style scoped>
 .countDatas {
+    width: 95%;
     margin: 10px;
     z-index: 2;
-    height: 200px;
+    flex-grow: 1;
     padding: 5px;
     overflow: hidden;
 }
@@ -135,7 +136,7 @@ onMounted(() => {
 }
 
 .visitContainer>div:hover {
-    background-color: rgb(241, 241, 241);
+    background-color: rgb(246, 245, 245);
 }
 
 .totalVisitContainer {
@@ -156,49 +157,21 @@ onMounted(() => {
     align-items: center;
 }
 
-.business {
+.chartView {
+    padding-left: 10px;
+    padding-right: 10px;
+    flex-grow: 3;
+    width: 95%;
     display: flex;
-    height: 580px;
-    background-color: azure;
+    justify-content: center;
+
 }
 
-.business>div {
-    padding: 5px;
-}
-
-.business>div>div {
-    padding: 5px;
-}
-
-.mainBusiness {
-    height: 100%;
-    width: 60%;
-    background-color: beige;
-}
-
-.productSale {
-    height: 70%;
-    background-color: aquamarine;
-}
-
-.commendCount {
-    height: 30%;
-    background-color: cadetblue;
-}
-
-.staffArea {
+.listContainer {
+    height: 380px;
+    margin-left: 10px;
+    background-color: white;
+    border-radius: 20px;
     width: 40%;
-    height: 100%;
-    background-color: blue;
-}
-
-.calendar {
-    height: 60%;
-    background-color: yellowgreen;
-}
-
-.schedule {
-    height: 40%;
-    background-color: violet;
 }
 </style>
---
+++ src/views/cms/components/VisitChart.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { NCard, NTabs, NTabPane } from 'naive-ui'
+
+const chartOptions = ref({ responsive: true })
+const chartData = ref({ labels: ['網頁', '食譜', '課程', '商品'], datasets: [{ label: '訪問統計', data: [40, 20, 12, 12] }] })
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+</script>
+<template>
+    <div>
+        <n-card style="border-radius: 20px;">
+            <n-tabs type="line" :animated="true">
+                <n-tab-pane name="項目" tab="項目">
+                    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+                </n-tab-pane>
+                <n-tab-pane name="月份" tab="月份">
+                    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+                </n-tab-pane>
+            </n-tabs>
+        </n-card>
+    </div>
+</template>
